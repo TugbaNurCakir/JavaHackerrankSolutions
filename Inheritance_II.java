b3de34852686e60270c32cc918142e8f91c43f59
tugbanur <46569497+TugbaNurCakir@users.noreply.github.com>
2021-10-06 23:57:07 +0300

    Create Inheritance_II.java

---
+++ Inheritance_II.java
@@ -0,0 +1,14 @@
+
+class Arithmetic{
+    
+public int add(int num1, int num2){
+        return num1 + num2;
+    }
+}
+class Adder extends Arithmetic{
+    public int callAdd(int num1, int num2){
+        return num1+num2;
+    }
+    
+    
+}
